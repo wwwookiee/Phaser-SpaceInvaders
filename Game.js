13f2bebc8b0eda526412745796e7c475ad5d4e69
wwwookiee <jordandivita@gmail.com>
2014-10-08 16:33:04 +0200

    preparing game over and win states

--- Game.js
+++ Game.js
@@ -1,9 +1,10 @@
 SpaceInvaders.Game = function(game) {
-	this.bulletTime = 0.1;
 	this.invaders;
 	this.totalRow;
 	this.totalInvadersRow;
+	this.ship;
 	this.bullets;
+	this.bulletTime = 500;
 };
 
 
@@ -13,6 +14,7 @@ SpaceInvaders.Game.prototype = {
 		this.physics.startSystem(Phaser.Physics.ARCADE);
 		this.totalRow = 4;
 		this.totalInvadersRow = 7;
+		this.totalInvaders= this.totalRow*this.totalInvadersRow;
 		this.buildWorld();
 		this.input.keyboard.addKeyCapture([ Phaser.Keyboard.LEFT, Phaser.Keyboard.RIGHT, Phaser.Keyboard.SPACEBAR ]);
 	},
@@ -44,23 +46,10 @@ SpaceInvaders.Game.prototype = {
 		    				invader.animations.add('invader02', this.game.math.numberArray(49,95));
 		        			invader.animations.play('invader02', 24, true);
 		    			}
-		    // 		switch(i){
-		    // 			case (i==0):
-		    // 				invader.animations.add('invader01', this.game.math.numberArray(1,48));
-		    //     			invader.animations.play('invader01', 24, true);
-		    //     			break;
-						// case (i==1 || i==2):
-		    // 				invader.animations.add('invader03', this.game.math.numberArray(96,143));
-		    //     			invader.animations.play('invader03', 24, true);
-		    //     			break;
-		    //     		case (i==3):
-		    // 				invader.animations.add('invader02', this.game.math.numberArray(49,95));
-		    //     			invader.animations.play('invader02', 24, true);
-		    //     	}
 		    	}
 		    }     
 	//  All this does is basically start the invaders moving. Notice we're moving the Group they belong to, rather than the invaders directly.
-    var tween = this.add.tween(this.invaders).to( { x: 60 }, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true);
+    var tween = this.add.tween(this.invaders).to( { x: 60 }, 750, Phaser.Easing.Linear.None, true, 0, 500, true);
 
     //  When the tween loops it calls descend
     tween.onLoop.add(this.descend, this);
@@ -71,10 +60,10 @@ SpaceInvaders.Game.prototype = {
 	},
 
 	buildShip: function(){
-		ship = this.add.sprite(270, 900, 'ship');
-		ship.enableBody = true;
-		ship.anchor.setTo(0.5, 0.5);
-		this.physics.enable(ship, Phaser.Physics.ARCADE);
+		this.ship = this.add.sprite(270, 900, 'ship');
+		this.ship.enableBody = true;
+		this.ship.anchor.setTo(0.5, 0.5);
+		this.physics.enable(this.ship, Phaser.Physics.ARCADE);
 	},
 
 	buildBullets: function(){
@@ -107,9 +96,9 @@ SpaceInvaders.Game.prototype = {
 
 	        if (bullet)
 	        {
-	            bullet.reset(ship.x, ship.y-25);
+	            bullet.reset(this.ship.x, this.ship.y-25);
 	            bullet.body.velocity.y = -300;
-	            this.bulletTime = this.time.now + 250;
+	            this.bulletTime = this.time.now + 500;
 	        }
 	    }
 
@@ -117,43 +106,56 @@ SpaceInvaders.Game.prototype = {
 	
 	//  Called if the bullet goes out of the screen
 	 resetBullet: function(bullet) {
-	 	console.log('bullet destroyed! Motafuka');
+	 	// console.log('bullet destroyed! Motafuka');
 	    bullet.kill();
 	},
 
 
 
-	collisionHandler: function (bullet, invader) {
-		console.log('aie, tu m\'as cogné du con !')
+	collisionBulletInvader: function (bullet, invader) {
+		// console.log('aie, tu m\'as cogné du con !');
     	bullet.kill();
    		invader.kill();
+   		this.totalInvaders--;
+   		if(this.totalInvaders == 0){
+   			console.log('You Win');
+   		}
 	},
+
+	collisionShipInvader: function (ship, invader) {
+			// console.log('aie, tu m\'as cogné du con !');
+	    	
+	   		
+	   			console.log('GAME OVER !');
+	   		
+		},
 
 	update: function() {
 
 		//Ship handler
-		ship.body.velocity.x = 0;
-	    ship.body.velocity.y = 0;
+		this.ship.body.velocity.x = 0;
+	    this.ship.body.velocity.y = 0;
 	    
 
-	    if (this.input.keyboard.isDown(Phaser.Keyboard.LEFT) && ship.body.x > 0)
+	    if (this.input.keyboard.isDown(Phaser.Keyboard.LEFT) && this.ship.body.x > 0)
 	    {
 	        
-	        ship.body.velocity.x = -200;
+	        this.ship.body.velocity.x = -200;
 	    }
-	    else if (this.input.keyboard.isDown(Phaser.Keyboard.RIGHT) && ship.body.x < 484)
+	    else if (this.input.keyboard.isDown(Phaser.Keyboard.RIGHT) && this.ship.body.x < 484)
 	    {
-	        ship.body.velocity.x = 200;
+	        this.ship.body.velocity.x = 200;
 	    }
 
 	    if (this.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR))
 	    {
 	        this.fireBullet();
+
 	    }
 
 	    // collision
-	    //  As we don't need to exchange any velocities or motion we can the 'overlap' check instead of 'collide'
-   		this.physics.arcade.overlap(this.bullets, this.invaders, this.collisionHandler, null, this);
+   		this.physics.arcade.overlap(this.bullets, this.invaders, this.collisionBulletInvader, null, this);
+   		this.physics.arcade.overlap(this.ship, this.invaders, this.collisionShipInvader, null, this);
 	}
 
 };
